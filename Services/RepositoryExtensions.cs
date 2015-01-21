eff00ec44d526165e2e773dc4b28c5ec4ac0a08e
Kegan Maher <kegan.maher@smgov.net>
2015-01-21 13:14:14 -0800

    adding some repo helpers

---
+++ Services/RepositoryExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CSM.ParkingData.Models;
+using Orchard.Data;
+
+namespace CSM.ParkingData.Services
+{
+    public static class RepositoryExtensions
+    {
+        public static MeteredSpace GetByMeterId(this IRepository<MeteredSpace> repository, string meterId)
+        {
+            return repository.Get(m => m.MeterId.Equals(meterId, StringComparison.Ordinal));
+        }
+
+        public static SensorEvent GetByTransmissionId(this IRepository<SensorEvent> repository, long transmissionId)
+        {
+            return repository.Get(s => s.TransmissionId == transmissionId);
+        }
+    }
+}
