a4859d568857c927099b3a3ed05794cd81f6ce30
Kegan Maher <kegan.maher@smgov.net>
2015-04-08 14:06:32 -0700

    renaming the setting to be more explicit

--- Models/SensorEventSettings.cs
+++ Models/SensorEventSettings.cs
@@ -4,10 +4,10 @@ namespace CSM.ParkingData.Models
 {
     public class SensorEventsSettings : ContentPart
     {
-        public double TimeLimitHours
+        public double LifetimeHours
         {
-            get { return this.Retrieve(x => x.TimeLimitHours, 3.0); }
-            set { this.Store(x => x.TimeLimitHours, value); }
+            get { return this.Retrieve(x => x.LifetimeHours, 3.0); }
+            set { this.Store(x => x.LifetimeHours, value); }
         }
     }
 }
