2dc5ead53b9c005be30f7c68c71b66cc1ef05dd9
Kegan Maher <kegan.maher@smgov.net>
2015-05-07 15:37:27 -0700

    define the data model and simple service layer

---
+++ Services/ParkingLotService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using CSM.ParkingData.ViewModels;
+using Microsoft.WindowsAzure;
+
+namespace CSM.ParkingData.Services
+{
+    public class ParkingLotService : IParkingLotService
+    {
+        public IEnumerable<ParkingLot> Get()
+        {
+            var parkingServerUrl = CloudConfigurationManager.GetSetting("ParkingLotServerUrl");
+            return Get(parkingServerUrl);
+        }
+
+        public IEnumerable<ParkingLot> Get(string parkingServerUrl)
+        {
+            var xdocument = XDocument.Load(parkingServerUrl);
+            var lots = new List<ParkingLot>();
+
+            foreach (var lot in xdocument.Root.Elements("lot"))
+            {
+                lots.Add(new ParkingLot() {
+                    AvailableSpaces = Convert.ToInt32(lot.Element("available").Value),
+                    Description = lot.Element("description").Value,
+                    Latitude = Convert.ToDecimal(lot.Element("latitude").Value),
+                    Longitude = Convert.ToDecimal(lot.Element("longitude").Value),
+                    Name = lot.Element("name").Value,
+                    StreetAddress = lot.Element("address").Value,
+                    ZipCode = Convert.ToInt16(lot.Element("zip").Value)
+                });
+            }
+
+            return lots;
+        }
+    }
+}
---
+++ ViewModels/ParkingLot.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace CSM.ParkingData.ViewModels
+{
+    [DataContract(Name = "lot", Namespace = "")]
+    public class ParkingLot
+    {
+        [DataMember(Name = "available_spaces")]
+        public int AvailableSpaces { get; set; }
+
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
+
+        [DataMember(Name = "latitude")]
+        [Range(-90.0, 90.0)]
+        public decimal? Latitude { get; set; }
+
+        [DataMember(Name = "longitude")]
+        [Range(-180.0, 180.0)]
+        public decimal? Longitude { get; set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "street_address")]
+        public string StreetAddress { get; set; }
+
+        [DataMember(Name = "zip_code")]
+        public int ZipCode { get; set; }
+    }
+}
