52d82900d867d0a3488866a38986316cfd8c5164
Kegan Maher <kegan.maher@gmail.com>
2015-05-07 21:45:11 -0700

    last update in utc

--- Services/ParkingLotsService.cs
+++ Services/ParkingLotsService.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using CSM.ParkingData.ViewModels;
+using Microsoft.WindowsAzure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
-using CSM.ParkingData.ViewModels;
-using Microsoft.WindowsAzure;
 
 namespace CSM.ParkingData.Services
 {
@@ -11,39 +11,59 @@ namespace CSM.ParkingData.Services
     {
         public IEnumerable<ParkingLot> Get()
         {
-            var lotDataUrl = CloudConfigurationManager.GetSetting("ParkingLotDataUrl");
-            return Get(lotDataUrl);
-        }
-
-        public IEnumerable<ParkingLot> Get(string lotDataUrl)
-        {
+            string lotDataUrl = CloudConfigurationManager.GetSetting("ParkingLotDataUrl");
             XDocument xdocument;
 
             try
             {
                 xdocument = XDocument.Load(lotDataUrl);
+                return Get(xdocument);
             }
             catch
             {
                 return Enumerable.Empty<ParkingLot>();
             }
+        }
 
-            var lots = new List<ParkingLot>();
+        public IEnumerable<ParkingLot> Get(XDocument lotXml)
+        {
+            var parkingLots = new List<ParkingLot>();
+            DateTime lastUpdateUtc = ParseLastUpdateUtc(lotXml.Root);
 
-            foreach (var lot in xdocument.Root.Elements("lot"))
+            foreach (var lot in lotXml.Root.Elements("lot"))
             {
-                lots.Add(new ParkingLot() {
-                    AvailableSpaces = Convert.ToInt32(lot.Element("available").Value),
-                    Description = lot.Element("description").Value,
-                    Latitude = Convert.ToDecimal(lot.Element("latitude").Value),
-                    Longitude = Convert.ToDecimal(lot.Element("longitude").Value),
-                    Name = lot.Element("name").Value,
-                    StreetAddress = lot.Element("address").Value,
-                    ZipCode = Convert.ToInt32(lot.Element("zip").Value)
-                });
+                var parkingLot = ParseFromXml(lot);
+                parkingLot.LastUpdate = lastUpdateUtc;
+                parkingLots.Add(parkingLot);
             }
 
-            return lots;
+            return parkingLots;
+        }
+
+        public ParkingLot ParseFromXml(XElement xml)
+        {
+            return new ParkingLot()
+            {
+                AvailableSpaces = Convert.ToInt32(xml.Element("available").Value),
+                Description = xml.Element("description").Value,
+                Latitude = Convert.ToDecimal(xml.Element("latitude").Value),
+                Longitude = Convert.ToDecimal(xml.Element("longitude").Value),
+                Name = xml.Element("name").Value,
+                StreetAddress = xml.Element("address").Value,
+                ZipCode = Convert.ToInt32(xml.Element("zip").Value)
+            };
+        }
+
+        public DateTime ParseLastUpdateUtc(XElement xml)
+        {
+            string dateString = xml.Element("date").Value;
+            string timeString = xml.Element("time").Value;
+            string tzString = xml.Element("timezone").Value;
+            
+            DateTime local = DateTime.ParseExact(dateString + " " + timeString, "yyyy-MM-dd HH:mm", null);
+            TimeZoneInfo sourceTz = TimeZoneInfo.FindSystemTimeZoneById(tzString);
+
+            return TimeZoneInfo.ConvertTimeToUtc(local, sourceTz);
         }
     }
 }
