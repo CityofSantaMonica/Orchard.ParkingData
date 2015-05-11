009e1dc47acd03e5c4ef729f02c2c6e882dd7502
Kegan Maher <kegan.maher@smgov.net>
2015-05-11 12:04:37 -0700

    refactor some common route data

---
+++ Routes/Routes.cs
@@ -0,0 +1,14 @@
+﻿namespace CSM.ParkingData.Routes
+{
+    internal static class Routes
+    {
+        public static readonly string Area = "CSM.ParkingData";
+
+        public static readonly string BaseEventRoute = "meters/events";
+        public static readonly string BaseEventAtRoute = "meters/{meterId}/events";
+
+        public static readonly int DefaultPriority = 5;
+
+        public static readonly string DateTimeConstraint = @"\d{8}T\d{6}Z";
+    }
+}
